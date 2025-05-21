9d5238092304e26caaea76b867f0fa9c9907c021
brucemaxx <silvabruce777@gmail.com>
2025-05-21 17:36:54 -0300

    Atualizando o Desafio Bancário
    Cumprindo desafio.

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -13,54 +13,64 @@ extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 
+# Função para realizar depósito
+def depositar(saldo, extrato):
+    valor = float(input("Informe o valor de depósito: ")) # Captura valor de deposito
+    if valor > 0:
+        saldo += valor  # Atualiza o saldo com o valor do depósito
+        extrato += f"Depósito: R$ {valor:.2f}\n" # Adciona o depósito ao extrato
+        print("Depósito realizado com sucesso!")
+    else:
+        print("Operação falhou! O valor informado é inválido.")
+    return saldo, extrato
+
+# Função para realizar saque
+def sacar(saldo, limite, numero_saques, LIMITE_SAQUE, extrato):
+    valor = float(input("Informe o valor do saque: ")) #Captura o valor do saque
+    if valor > saldo:
+        print("Operação falhou! Você não tem saldo suficiente.")
+    elif valor > limite:
+        print(f"Operação falhou! O valor do saque execede o limite de R$ {limite}.") 
+    elif numero_saques >= LIMITE_SAQUE:
+        print("Operação falhou! Número máximo de saques diários excedido.")
+    elif valor > 0:
+        saldo -= valor # Atualiza o saldo decontando o valor do saque
+        extrato += f"Saque: R$ {valor:.2f}\n" # Adicona o saque ao extrato
+        numero_saques += 1 # Incrementar o contador de saques
+        print("Saque realizado com sucesso!")
+    else:
+        print("Operacação falhou! O valor informado é inválido.")
+    return saldo, numero_saques, extrato
+
+# Função para exibir extrato
+def exibir_extrato(saldo, extrato):
+    print("\n================ EXTRATO ================")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo: R$ {saldo:.2f}") # Exibe o saldo atual
+    print("==========================================")
+    
+# Loop principal que mantém o programa em execução
 while True:
 
     opcao = input(menu)
 
+    # Chama a função de depósito
     if opcao == "d":
-        valor = float(input("Informe o valor do depósito: "))
+        saldo, extrato = depositar(saldo, extrato)
 
-        if valor > 0:
-            saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}\n"
-
-        else:
-            print("Operação falhou! O valor informado é inválido.")
-
+    # Chama a função de saque
     elif opcao == "s":
-        valor = float(input("Informe o valor do saque: "))
+        saldo, numero_saques, extrato = sacar(saldo, limite, numero_saques, LIMITE_SAQUES, extrato)
 
-        excedeu_saldo = valor > saldo
-
-        excedeu_limite = valor > limite
-
-        excedeu_saques = numero_saques >= LIMITE_SAQUES
-
-        if excedeu_saldo:
-            print("Operação falhou! Você não tem saldo suficiente.")
-
-        elif excedeu_limite:
-            print("Operação falhou! O valor do saque excede o limite.")
-
-        elif excedeu_saques:
-            print("Operação falhou! Número máximo de saques excedido.")
-
-        elif valor > 0:
-            saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
-            numero_saques += 1
-
-        else:
-            print("Operação falhou! O valor informado é inválido.")
-
+# Chama a função de exibir extrato
     elif opcao == "e":
-        print("\n================ EXTRATO ================")
-        print("Não foram realizadas movimentações." if not extrato else extrato)
-        print(f"\nSaldo: R$ {saldo:.2f}")
-        print("==========================================")
-
+        exibir_extrato(saldo, extrato)
+        
+# Finalização de programa
     elif opcao == "q":
+        print("Obrigado por usar o sistema bancário!")
         break
 
+# Tratamento de entradas inválidas
     else:
         print("Operação inválida, por favor selecione novamente a operação desejada.")
